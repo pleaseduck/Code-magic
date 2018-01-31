9b09444d3ef7aa19170e57b3cfcc96ebeacaf20f
pleaseduck <pikov.artem@icloud.com>
2018-01-31 21:57:53 +0300

    Добавил интерактивности
    Окошко установки персонажа открывается и закрывается, по нажатию на основного волшебника генерируется новый

--- js/setup.js
+++ js/setup.js
@@ -5,7 +5,6 @@ var wizardСharacteristics = {
   'coat': ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
   'eyesColor':  ['black', 'red', 'blue', 'yellow', 'green']
 };
-setupBlock.classList.remove('hidden');
 
 var wizardsList = document.querySelector('.setup-similar-list');
 
@@ -19,16 +18,50 @@ for (var i = 0; i < 4; i++) {
   wizardsList.appendChild(similarWizardTemplate.content.cloneNode(true));
   console.log(similarWizardTemplate.content.querySelector('.setup-similar-label'))
 }
-/*
-for (var i = 0; i < 4 ; i++) {
-var wizardName = similarWizardTemplate.querySelector('.setup-similar-label').innerHTML;
-wizardName = wizardСharacteristics.name[randomNumber(wizardСharacteristics.name)] +
-wizardСharacteristics.surname[randomNumber(wizardСharacteristics.surname)];
-var wizardElem = similarWizardTemplate.cloneNode(true);
-wizardsList.appendChild(wizardElem);
+var setupOpener = document.querySelector('.setup-open');
+var setupCloser = document.querySelector('.setup-close');
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+var openSetup = function () {
+    setupBlock.classList.remove('hidden');
+    document.addEventListener('keydown', function(evt) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        closeSetup();
+      }
+    });
+};
+var closeSetup = function () {
+    setupBlock.classList.add('hidden');
+};
 
-console.log( wizardName)
-}*/
+setupOpener.addEventListener('click', openSetup);
+
+setupCloser.addEventListener('click', closeSetup);
+
+setupOpener.addEventListener('keydown',function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openSetup();
+  }
+});
+
+setupCloser.addEventListener('keydown',function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closeSetup();
+  }
+});
+  var wizardSetup = document.querySelector('.setup-player');
+  var fireballColors = ['#ee4830',' #30a8ee', '#5ce6c0', '#e848d5', '#e6e848']
+
+  wizardSetup.addEventListener('click', function() {
+    randomWizard();
+  });
+  console.log(randomWizard)
+  function randomWizard() {
+  var wizardFireball = document.querySelector('.setup-fireball');
+  wizardSetup.querySelector('.wizard-coat').style.fill = wizardСharacteristics.coat[randomNumber(wizardСharacteristics.coat)];
+  wizardSetup.querySelector('.wizard-eyes').style.fill = wizardСharacteristics.eyesColor[randomNumber(wizardСharacteristics.eyesColor)];
+  wizardFireball.style.backgroundColor = fireballColors[randomNumber(fireballColors)];
+}
   function randomNumber(array) {
   return Math.floor(Math.random()*array.length);
 }
